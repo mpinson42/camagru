090c844b9b9918bc2b686cf23aa41f57400c864f
Mathieu PINSON <mpinson@e1r5p19.42.fr>
2018-05-21 20:54:41 +0200

    ok

---
+++ config/setup.php
@@ -0,0 +1,26 @@
+<?php
+include('database.php');
+header('Content-Type: application/json');
+
+
+function reponse_json($success, $data, $msgErreur=NULL) {
+	$array['success'] = $success;
+	$array['msg'] = $msgErreur;
+	$array['result'] = $data;
+	if(!empty($_SESSION['logged_on_user']))
+	$array['loged'] = $_SESSION['logged_on_user'];
+
+	echo json_encode($array);
+}
+
+$requete = $pdo->prepare("DROP DATABASE api");
+$requete->execute();
+
+$requete = $pdo->prepare("CREATE DATABASE api");
+$requete->execute();
+
+$requete = $pdo->prepare("CREATE TABLE `api`.`user` ( `login` TEXT NOT NULL , `passwd` TEXT NOT NULL , `email` TEXT NOT NULL , `id` TEXT NOT NULL , `img_id` TEXT NOT NULL , `mail` INT NOT NULL ) ENGINE = InnoDB;");
+$requete->execute();
+
+$requete = $pdo->prepare("CREATE TABLE `api`.`img` ( `path` TEXT NOT NULL , `id` TEXT NOT NULL , `likedby` TEXT NOT NULL , `commentby` TEXT NOT NULL , `creat_by` TEXT NOT NULL ) ENGINE = InnoDB;");
+$requete->execute();
